722dd5fe2e9a884b92bb98eb5381faab527e367f
JoshAnnan <annanjosh04@gmail.com>
2022-04-28 15:36:27 -0700

    nextAuth authentication

--- pages/posts/[postId].jsx
+++ pages/posts/[postId].jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { useRouter } from "next/router";
+import React from 'react';
+import { useRouter } from 'next/router';
 
 const PostDetail = () => {
-  const router = useRouter();
-  const { postId } = router.query;
+    const router = useRouter()
+    const { postId } = router.query;
+    console.log(router);
+  return (
+    <div>Post Detail - {postId}</div>
+  )
+}
 
-  console.log(router);
-
-  return <div>Post Detail - {postId}</div>;
-};
-
-export default PostDetail;
+export default PostDetail
